2ce2dac1705ad482016882647a017c4ab3f1d868
yattinda <yattinda@gmail.com>
2021-12-07 09:15:29 +0900

    Say hello

--- discordbot.py
+++ discordbot.py
@@ -2,10 +2,18 @@ import discord
 from seacret import bot_token
 
 client = discord.Client()
-
 @client.event
 async def on_ready():
-    print("on_ready")
-    print(discord.__version__)
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("Ahi"):
+        if client.user != message.author:
+            m = "おはようございます" + message.author.name + "さん！"
+            await message.channel.send(m)
 
 client.run(bot_token)
