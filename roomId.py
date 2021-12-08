f32df8193ae34c9663b2613f4046c8c72acb1a52
yattinda <yattinda@gmail.com>
2021-12-08 11:58:20 +0900

    Add File

---
+++ roomId.py
@@ -0,0 +1,23 @@
+voiceChannel = {
+    "virtual-216" : 720631179442913300,
+    "old-member-room" : 702804427928502353,
+    "project-room-1" : 703494244907614270,
+    "project-room-2" : 710822318150254642,
+    "project-room-3" : 710822359090724874,
+    "project-room-4" : 710822522060275733,
+    "project-room-5" : 738698472206762014,
+}
+
+textChannel = {
+    "virtual-216" : 700248089214320673,
+    "attend-list" : 826086746130219068,
+    "groovy-bot"  : 702084734053908480,
+    "zoom-link-share" : 719559661530906704,
+    "shift-list" : 756459926288859176,
+    "old-member-room" : 702804840023064686,
+    "project-room-1" : 703494338130214942,
+    "project-room-2" : 710822559545032756,
+    "project-room-3" : 710822602230464603,
+    "project-room-4" : 710822634966876171,
+    "project-room-5" : 738698239544524872,
+}
