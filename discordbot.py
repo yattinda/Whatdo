e241f18fd8b9aba0a6ecb58ed6be6fa0e3b24c29
yattinda <yattinda@gmail.com>
2021-12-25 23:13:57 +0900

    Add button and interaction

--- discordbot.py
+++ discordbot.py
@@ -16,11 +16,20 @@ async def on_ready():
 @client.event
 async def on_voice_state_update(member, before, after):
     if before.channel != after.channel:
+        #botが書き込むチャンネル
         botRoom = client.get_channel(777506677624799245)
+        #監視対象のボイスチャンネル
         announceChannelIds = [777506678068477952]
 
         if after.channel is not None and after.channel.id in announceChannelIds:
-            await botRoom.send(after.channel.name + "に" + member.name + "が参加！")
+            view = discord.ui.View()
+            view.add_item(discord.ui.Button(style=discord.ButtonStyle.green, label="test"))
+            await botRoom.send("これはボタンです。", view=view)
+
+@client.event
+async def on_interaction(interaction):
+    await interaction.channel.send("Interactionが発生しました。")
+    await interaction.channel.send("id:{}\ntype:{}".format(interaction.id, interaction.type))
 
 load_dotenv()
 client.run(os.getenv('BOT_TOKEN'))
