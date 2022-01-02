b63de5a0c2d0d691e9e43b7cb9487ae6047f668b
yattinda <yattinda@gmail.com>
2022-01-02 21:11:16 +0900

    Fix at flake8

--- discordbot.py
+++ discordbot.py
@@ -9,6 +9,7 @@ import words
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     print('Logged in as')
@@ -16,22 +17,24 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
+
 @client.event
 async def on_voice_state_update(member, before, after):
     if before.channel != after.channel:
-        #botが書き込むチャンネル
+        # botが書き込むチャンネル
         botRoom = client.get_channel(777506677624799245)
-        #監視対象のボイスチャンネル
+        # 監視対象のボイスチャンネル
         announceChannelIds = [777506678068477952]
 
         if after.channel is not None and after.channel.id in announceChannelIds:
             join_mes = random.choice(words.F_words) + random.choice(words.L_words)
-            await botRoom.send("ようこそ! " + join_mes+ " " + member.display_name)
+            await botRoom.send("ようこそ! " + join_mes + " " + member.display_name)
             view = discord.ui.View()
             view.add_item(discord.ui.Button(style=discord.ButtonStyle.danger, custom_id="free", label="暇！"))
             view.add_item(discord.ui.Button(style=discord.ButtonStyle.primary, custom_id="question", label="質問したい！"))
             view.add_item(discord.ui.Button(style=discord.ButtonStyle.secondary, custom_id="notnotifier", label="通知しない"))
             await botRoom.send("何をしていますか？？？？", view=view)
+
 
 @client.event
 async def on_message(message):
@@ -41,6 +44,7 @@ async def on_message(message):
     if "何をしていますか？？？？" in message.content:
         await asyncio.sleep(120)
         await message.delete()
+
 
 @client.event
 async def on_interaction(interaction):
