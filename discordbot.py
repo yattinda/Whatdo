2e9f9a1d4693c84ff19bcb8ec437e0ef93f51a81
yattinda <yattinda@gmail.com>
2022-01-07 00:04:31 +0900

    Test workflows

--- discordbot.py
+++ discordbot.py
@@ -17,7 +17,6 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
-
 @client.event
 async def on_voice_state_update(member, before, after):
     if before.channel != after.channel:
